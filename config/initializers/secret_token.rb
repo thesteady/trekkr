04ad1303d350706459daa6a03077283239aa5d14
Laura Steadman <lauramsteadman@gmail.com>
2013-05-29 17:26:54 -0600

    starting up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trekkr::Application.config.secret_token = 'e35878ea5963d69b5b33f06a670d053e5ed74b897f894f242dd1249333fa4c2d27a8505500caedcf661171b6052d8128adef874c5c8e155b1811135f15a4bc1b'
