9f43a3482378476587c43698dfd5bcd82a880c26
Laura Steadman <lauramsteadman@gmail.com>
2013-06-04 20:30:31 -0600

    minor edits to map.js and readme

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -14,13 +14,18 @@ var configureMap = function() {
       e.layer.unbindPopup();
 
       var feature = e.layer.feature;
+      // set the text in the sidebar
       var info =
                   '<img class="photo" src="' + feature.properties.url + '" width="325">' +
                   '<p class="credit"> Photo Credit: Instagram user <span class="username">' + feature.properties.title + '</span></p>' +
                  '<div class="more">' + feature.properties.description + '</div>'
                  ;
-
       document.getElementById('info').innerHTML = info;
+
+      //highlight the clicked marker
+        // first reset any previous marker with the highlight
+        // then set the current marker to highlighted.
+
   });
 
   // Clear the tooltip when map is clicked
@@ -30,6 +35,6 @@ var configureMap = function() {
       document.getElementById('info').innerHTML = '';
   });
       });
-    };
+};
   
   $(document).ready(configureMap);
